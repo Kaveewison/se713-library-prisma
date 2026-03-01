1f9502f252acfba70039c86671368bceab61afed
Kaveewison <kaveewison_k@cmu.ac.th>
2026-03-01 21:23:15 +0700

    add endpoints success

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,21 @@
+// import express from "express";
+
+// const app = express();
+// app.use(express.json());
+
+// app.get("/", (_req, res) => {
+//   res.send("Library API is running");
+// });
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
 import express from "express";
+import bookRoute from "./routes/BookRoute";
+import authorRoute from "./routes/AuthorRoute";
+import memberRoute from "./routes/MemberRoute";
 
 const app = express();
 app.use(express.json());
@@ -6,6 +23,10 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("Library API is running");
 });
+
+app.use("/books", bookRoute);
+app.use("/authors", authorRoute);
+app.use("/members", memberRoute);
 
 const port = 3000;
 app.listen(port, () => {
