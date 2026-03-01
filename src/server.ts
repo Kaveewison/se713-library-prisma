942e40785f062f4296b6fe700cd9bfab6548dda5
Kaveewison <kaveewison_k@cmu.ac.th>
2026-03-01 21:26:17 +0700

    delete comment

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,3 @@
-// import express from "express";
-
-// const app = express();
-// app.use(express.json());
-
-// app.get("/", (_req, res) => {
-//   res.send("Library API is running");
-// });
-
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
-
 import express from "express";
 import bookRoute from "./routes/BookRoute";
 import authorRoute from "./routes/AuthorRoute";
